16190bef89ee73b02a1744c3b90e3a8d28c4176d
Nath Meriaux <nath.meriaux@gmail.com>
2016-08-11 15:04:16 +0200

    Add File to Videos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810092033) do
+ActiveRecord::Schema.define(version: 20160811125850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160810092033) do
     t.string   "realisation"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "file"
   end
 
 end
